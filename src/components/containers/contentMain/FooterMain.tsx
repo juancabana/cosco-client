d4178fc3d36c2ec622f0c46ccdd1d1dec0682c3b
Juan Cabana <juan.cabana@pragma.com.co>
2024-08-25 12:56:31 -0500

    chore: Fontawesome library removed

--- src/components/containers/contentMain/FooterMain.tsx
+++ src/components/containers/contentMain/FooterMain.tsx
@@ -5,7 +5,7 @@ const FooterMain = () => {
     <footer className="bg-[#134E4A] text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} COSCO. Todos los derechos reservados.</p>
-        <div className="mt-4 flex justify-center gap-4">
+        {/* <div className="mt-4 flex justify-center gap-4">
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-facebook-f text-xl"></i>
           </a>
@@ -18,7 +18,7 @@ const FooterMain = () => {
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin-in text-xl"></i>
           </a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
--- src/components/containers/contentMain/index.tsx
+++ src/components/containers/contentMain/index.tsx
@@ -3,7 +3,6 @@ import CardMain from "./cardMain";
 import CardInvitation from "./cardInvitation";
 import CardHistory from "./cardHistory";
 import FooterMain from "./FooterMain";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 const ContentMain = () => (
