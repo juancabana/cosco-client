44b631e5ab4c0fd6e5b8cbd418107d86ade1dfc7
Juan Cabana <juan.cabana@pragma.com.co>
2024-09-15 16:42:34 -0500

    api url

--- src/api/cosco.api.ts
+++ src/api/cosco.api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const coscoApi = axios.create({
-  // baseURL: process.env.GATSBY_API_URL || "http://localhost:3000/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.GATSBY_API_URL || "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
