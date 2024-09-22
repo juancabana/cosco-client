447e18daf41f944a4881f1fcc5b2a50965f6cd35
Juan Cabana <juan.cabana@pragma.com.co>
2024-09-22 18:17:12 -0500

    head style

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,6 +26,9 @@ export default {
           "Poppins:100,200,300,400,500,600,700,800,900",
         ],
         display: "swap",
+        attributes: {
+          rel: "stylesheet preload prefetch",
+        },
       },
     },
     {
